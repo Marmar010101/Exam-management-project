7cab12bafcb83d2d65b123a1fa8866ce5f9b21c8
Marwabelkacemo <marwabelkacemi88@gmail.com>
2026-01-04 14:47:10 +0100

    Complete merge of meriem-feature seeders
     SUCCESSFULLY MERGED ALL SEEDERS FROM MERIEM-FEATURE:
    
     Academic Structure:
    - CycleSeeder, LevelSeeder, SemesterSeeder, SpecialitySeeder
    - GroupSeeder, ModuleSeeder, RoomSeeder
    
     Users & Roles:
    - TeacherSeeder, StudentSeeder, ResponsableSeeder, HeadDepartmentSeeder
    - TeacherModuleSeeder, TeacherExamSeeder (additional teacher data)
    
     Planning & Management:
    - ExamSeeder (exams with correct column names)
    - InvigilationScheduleSeeder (surveillance planning)
    
     Technical Fixes:
    - Removed incompatible UserDataSeeder
    - Fixed GroupSeeder (removed students_count)
    - All seeders now compatible with database structure
    - Complete seeding successful with 15 seeders
    
     Database fully populated with:
    - Academic cycles, levels, semesters, specialities
    - Groups, modules, rooms, exams
    - Teachers, students, responsables, head departments
    - Teacher assignments and surveillance schedules
    
    Ready for production use!

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -37,6 +37,8 @@ class DatabaseSeeder extends Seeder
             ResponsableSeeder::class,
             ExamSeeder::class,
             InvigilationScheduleSeeder::class,
+            TeacherModuleSeeder::class,
+            TeacherExamSeeder::class,
         ]);
     }
 }
